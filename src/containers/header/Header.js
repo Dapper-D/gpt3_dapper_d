8bb8d1d5ad913455a59757dd223c964b06d02ff0
Dapper-d <oladapoomoworare@gmail.com>
2023-01-12 10:30:01 +0100

    updated header

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
   return (
@@ -8,6 +10,27 @@ const Header = () => {
         <h1 className="gradient__text">
           Let's Build Something amazing with GPT-3 OpenAI
         </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
+          alias voluptas reprehenderit eos veniam aut animi ipsam ipsum iusto
+          delectus fuga quod tenetur quia, dolorem quos incidunt illum corrupti
+          sint.
+        </p>
+
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button typ e="button">
+            {" "}
+            Get Started
+          </button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} />
       </div>
     </div>
   );
